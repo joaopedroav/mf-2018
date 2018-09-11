b265aba3c48ef4402df40a8c6cedbd2bbcab7d67
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-10 21:50:19 -0300

    Continuidade do exercício 11

--- aula-3/csv/Aplicacao.java
+++ aula-3/csv/Aplicacao.java
@@ -14,10 +14,15 @@ public class Aplicacao {
     public static final String LINHA_FINAL = "Fonte: MEC/Inep; Tabela elaborada pelo Inep/DEED";
     public static final String CAMINHO_ARQUIVO = "IES_2011.csv";
 
+    public static final int CABECALHO = 10;
+
     public static void main(String[] args) throws IOException {
         salvarArquivo(URL_CVS, CAMINHO_ARQUIVO);
-        //System.out.println(lerArquivo(CAMINHO_ARQUIVO));
         ArrayList<String> coluna = lerArquivo(CAMINHO_ARQUIVO);
+        System.out.println(coluna.get(1));
+        //for(int i = 0; i < 10; i++) {
+        //   System.out.println(coluna.get(i));
+        //}
     }
 
     public static void salvarArquivo(String link, String caminho) throws IOException {
@@ -40,13 +45,22 @@ public class Aplicacao {
     public static ArrayList<String> lerArquivo(String caminho) {
         BufferedReader conteudo = null;
         String linha = "";
-        ArrayList<String> coluna = new ArrayList<String>();
+        ArrayList<String> colunas = new ArrayList<String>();
+        String[] x = null;
+        int numLinhasArq = 0, i = 0;//index do ArrayList
         try {
             conteudo = new BufferedReader(new FileReader(caminho));
             while((linha = conteudo.readLine()) != null) {
-                coluna.add(linha);
+                if (numLinhasArq > CABECALHO) {
+                    colunas.add(linha);
+                    x = colunas.get(i).split(";");
+                    System.out.println(x[9]);//indices das siglas
+                    i++;
+                }
+                numLinhasArq++;
             }
         } catch (IOException e) {}
-        return coluna;
+        System.out.println("xxxxxxxxxxxxxx");
+        return colunas;
     }
 }
