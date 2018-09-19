5b417c60e4db17c53d5df156c860f441d5d8d186
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-19 16:28:55 -0300

    Add package-info

---
+++ aula-3/codigo/src/main/java/com/github/jops425/codigo/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do projeto mf-2018/aula-3/codigo. Contém uma classe apenas:
+ * ({@link com.github.jops425.codigo.ContaLinhas})
+ *
+ * @see com.github.jops425.codigo.ContaLinhas
+ */
+package com.github.jops425.codigo;
