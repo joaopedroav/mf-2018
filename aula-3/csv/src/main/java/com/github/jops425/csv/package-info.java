000de06fb26f80c33a03fd6ad3d91f9cf9ba2bb4
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-19 15:27:51 -0300

    Add package-info

---
+++ aula-3/csv/src/main/java/com/github/jops425/csv/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Pacote do projeto mf-2018/aula-3/csv. Contém três classes:
+ * ({@link com.github.jops425.csv.Aplicacao})
+ * ({@link com.github.jops425.csv.Estado})
+ * ({@link com.github.jops425.csv.SortByInst})
+ *
+ * @see com.github.jops425.csv.Aplicacao
+ * @see com.github.jops425.csv.Estado
+ * @see com.github.jops425.csv.SortByInst
+ */
+package com.github.jops425.csv;
