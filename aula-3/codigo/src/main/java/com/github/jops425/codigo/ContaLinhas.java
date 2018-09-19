b6d3c7fe191e734a960e68da7de73cc217136113
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-19 16:28:35 -0300

    Adequação ao checkstyle

--- aula-3/codigo/src/main/java/com/github/jops425/codigo/ContaLinhas.java
+++ aula-3/codigo/src/main/java/com/github/jops425/codigo/ContaLinhas.java
@@ -6,13 +6,31 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.File;
 
-public class ContaLinhas {
+/**
+ * Classe que conta o número de linhas de determinados arquivos.
+ *
+ * @author João Pedro
+ */
+public final class ContaLinhas {
 
-    public static void main(String[] args) {
+    /**
+     * Construtor.
+     */
+    private ContaLinhas() {
+
+    }
+
+
+    /**
+     * Método main.
+     *
+     * @param args Argumentos a serem passados por linha de comando.
+     */
+    public static void main(final String[] args) {
         int linhasTotais = 0;
         File files = new File(System.getProperty("user.dir"));
         ArrayList<String> lista = listarArquivos(files);
-        for(int i = 0; i < lista.size(); i++) {
+        for (int i = 0; i < lista.size(); i++) {
             int aux = contaLinhas(lista.get(i));
             System.out.println(aux + " " + lista.get(i));
             linhasTotais = linhasTotais + aux;
@@ -20,20 +38,34 @@ public class ContaLinhas {
         System.out.println("Total de linhas = " + linhasTotais);
     }
 
-    public static int contaLinhas(String caminho) {
+    /**
+     * Contar número de linhas de determinado arquivo.
+     *
+     * @param caminho Caminho do arquivo.
+     * @return A quantidade de linhas do arquivo.
+     */
+    public static int contaLinhas(final String caminho) {
         BufferedReader conteudo = null;
         ArrayList<String> linhas = new ArrayList<String>();
         String linha = "";
         try {
             conteudo = new BufferedReader(new FileReader(caminho));
-            while((linha = conteudo.readLine()) != null) {
+            while ((linha = conteudo.readLine()) != null) {
                 linhas.add(linha);
             }
-        } catch (IOException e) {}
+        } catch (IOException e) {
+
+        }
         return linhas.size();
     }
 
-    public static ArrayList<String> listarArquivos(File file) {
+    /**
+     * Listar arquivos existentes.
+     *
+     * @param file Caminho do arquivo a ser percorrido recursivamente.
+     * @return Lista de arquivos.
+     */
+    public static ArrayList<String> listarArquivos(final File file) {
         ArrayList<String> lista = new ArrayList<String>();
         try {
             File[] listaArquivos = file.listFiles();
@@ -43,8 +75,8 @@ public class ContaLinhas {
                 }
             }
         } catch (IOException e) {
-			e.printStackTrace();
-		}
-		return lista;
-	}
+            e.printStackTrace();
+        }
+        return lista;
+    }
 }
