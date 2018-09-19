63021e88c18ee8ac72848a3ed60220f11a8ef718
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-19 16:45:27 -0300

    Add package-info

---
+++ aula-3/dir/src/main/java/com/github/jops425/dir/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Pacote do projeto mf-2018/aula-3/dir. Contém duas classe apenas:
+ * ({@link com.github.jops425.dir.DiretorioListarArquivos})
+ * ({@link com.github.jops425.dir.DiretorioListarDirs})
+ *
+ * @see com.github.jops425.dir.DiretorioListarArquivos
+ * @see com.github.jops425.dir.DiretorioListarDirs
+ */
+package com.github.jops425.dir;
