f604d091fa6b78dc90f0c97cc68931890d43b11a
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-01 21:54:04 -0300

    Att package-info

---
+++ aula-5/cnes/src/main/java/com/github/jops425/cnes/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do projeto mf-2018/aula-5/cnes. Contém uma classe apenas:
+ * ({@link com.github.jops425.csv.Aplicacao})
+ *
+ * @see com.github.jops425.cnes.Main
+ */
+package com.github.jops425.csv;
