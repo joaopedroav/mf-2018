98a130a3434c9797439b6b6b08392c23e3b25daa
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-01 21:51:52 -0300

    Add classe Main

---
+++ aula-5/cnes/src/main/java/com/github/jops425/cnes/Main.java
@@ -0,0 +1,25 @@
+package com.github.jops425.cnes;
+
+/**
+ * Classe Main do projeto cnes.
+ * @author João Pedro
+ */
+public final class Main {
+
+    /**
+     * Construtor.
+     */
+    private Main() {
+
+    }
+
+    /**
+     * Método main.
+     *
+     * @param args Parâmetros a serem entrados na linha de comando.
+     */
+    public static void main(final String[] args) {
+        System.out.println(".");
+    }
+
+}
