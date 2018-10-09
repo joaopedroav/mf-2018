ade80ffae1e8a6bc31be44fdaf50ba3fb1627242
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-09 16:34:53 -0300

    Add classe Colunas

---
+++ aula-5/cnes/src/main/java/com/github/jops425/cnes/Colunas.java
@@ -0,0 +1,14 @@
+package com.github.jops425.cnes;
+
+/**
+ * Classe para extrair colunas do arquivo lido.
+ */
+public class Colunas {
+
+    /**
+     * Construtor.
+     */
+    private Colunas() {
+
+    }
+}
