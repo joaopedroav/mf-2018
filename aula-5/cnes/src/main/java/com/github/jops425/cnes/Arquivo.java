f9655c4d7b5bb9adfcc0ffd36b746ced1506e7f7
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-09 16:26:40 -0300

    Adicionados métodos para descompactar

--- aula-5/cnes/src/main/java/com/github/jops425/cnes/Arquivo.java
+++ aula-5/cnes/src/main/java/com/github/jops425/cnes/Arquivo.java
@@ -1,10 +1,15 @@
 package com.github.jops425.cnes;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.io.BufferedOutputStream;
 
 /**
  * Classe Arquivo do projeto cnes.
@@ -27,10 +32,10 @@ public final class Arquivo {
     /**
      * Baixa arquivo.
      *
-     * @param link A url contendo o arquivo.
+     * @param link    A url contendo o arquivo.
      * @param caminho Caminho do arquivo a ser baixado.
      */
-    public static void baixarArquivo(final String link, final String caminho) {
+    public static void baixarZip(final String link, final String caminho) {
         try {
             URL url = new URL(link);
             URLConnection conexao = url.openConnection();
@@ -46,10 +51,56 @@ public final class Arquivo {
 
             outputStream.close();
             inputStream.close();
-            System.out.println("Foi feito o download do arquivo .ZIP.");
+            System.out.println("Arquivo .zip baixado");
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    /**
+     * Método para descompactar zip.
+     *
+     * @param dir  Caminho corrente do zip.
+     * @param dest Diretório de destino da descompactação.
+     * @throws IOException Exceção.
+     */
+    public static void descompactar(final String dir, final String dest)
+    throws IOException {
+        File destDir = new File(dest);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zIS = new ZipInputStream(new FileInputStream(dir));
+        ZipEntry zipEntry = zIS.getNextEntry();
+
+        while (zipEntry != null) {
+            String filePath = dest + File.separator + zipEntry.getName();
+            if (!zipEntry.isDirectory()) {
+                extrair(zIS, filePath);
+            }
+            zIS.closeEntry();
+            zipEntry = zIS.getNextEntry();
+        }
+        zIS.close();
+    }
+
+    /**
+     * Método auxiliar para descompactar zip.
+     *
+     * @param zIS    Objeto tipo ZipInputStream.
+     * @param dirZip Diretório corrente do zip.
+     * @throws IOException Exceção.
+     */
+    private static void extrair(final ZipInputStream zIS, final String dirZip)
+    throws IOException {
+        BufferedOutputStream bOS =
+        new BufferedOutputStream(new FileOutputStream(dirZip));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zIS.read(bytesIn)) != -1) {
+            bOS.write(bytesIn, 0, read);
+        }
+        bOS.close();
     }
 }
--- aula-5/cnes/src/main/java/com/github/jops425/cnes/Main.java
+++ aula-5/cnes/src/main/java/com/github/jops425/cnes/Main.java
@@ -1,5 +1,7 @@
 package com.github.jops425.cnes;
 
+import java.io.IOException;
+
 /**
  * Classe Main do projeto cnes.
  * @author João Pedro
@@ -17,12 +19,16 @@ public final class Main {
      * Método main.
      *
      * @param args Parâmetros a serem entrados na linha de comando.
+     * @throws IOException Exceção de Input/Output.
      */
-    public static void main(final String[] args) {
-        String url =
-        "ftp://ftp.datasus.gov.br/cnes/BASE_DE_DADOS_CNES_201808.ZIP";
+    public static void main(final String[] args) throws IOException {
+        // String url = args[0];
         String caminho = "cnes.zip";
-        Arquivo.baixarArquivo(url, caminho);
+        // Arquivo.baixarZip(url, caminho);
+        //String caminhoZip = "tbEstabelecimento201808.csv";
+        String destinoZip = "baseDados";
+        Arquivo.descompactar(caminho, destinoZip);
+
     }
 
 }
