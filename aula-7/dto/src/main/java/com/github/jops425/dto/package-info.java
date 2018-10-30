36aab85742e9655d5066a16867ae7e669387cc96
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-30 18:29:59 -0300

    Add package-info

---
+++ aula-7/dto/src/main/java/com/github/jops425/dto/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do projeto mf-2018/aula-7/dto. Contém uma classe apenas:
+ * ({@link com.github.jops425.dto.Main})
+ *
+ * @see com.github.jops425.dto.Main
+ */
+package com.github.jops425.dto;
---
+++ aula-7/grpc/src/main/java/com/github/jops425/grpc/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do projeto mf-2018/aula-7/grpc. Contém uma classe apenas:
+ * ({@link com.github.jops425.grpc.Main})
+ *
+ * @see com.github.jops425.grpc.Main
+ */
+package com.github.jops425.grpc;
