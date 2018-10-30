ace7845924b68ba4af7a2d36ca40b655b834f2df
João Pedro Vieira <jp-a.v@hotmail.com>
2018-10-30 18:38:31 -0300

    Add interface

---
+++ aula-7/dto/src/main/java/com/github/jops425/dto/DTO.java
@@ -0,0 +1,30 @@
+package com.github.jops425.dto;
+
+/**
+ * Interface com os métodos DTO.
+ *
+ * @author joaopedro
+ *
+ */
+public interface DTO {
+
+	/**
+	 * Serializa json para objeto.
+	 */
+	void fromJson();
+
+	/**
+	 * Serializa objeto para json.
+	 */
+	void toJson();
+
+	/**
+	 * Serializa xml para objeto.
+	 */
+	void fromXML();
+
+	/**
+	 * Serializa objeto para xml.
+	 */
+	void toXML();
+}
