f181cca18976136b8fca916e9656918f9aa61ebc
João Pedro Vieira <jp-a.v@hotmail.com>
2018-11-05 20:54:09 -0200

    Add classes

---
+++ aula-7/dto/src/main/java/com/github/jops425/dto/NomeDTO.java
@@ -0,0 +1,41 @@
+package com.github.jops425.dto;
+
+public class NomeDTO implements DTO {
+
+    /* (non-Javadoc)
+     * @see com.github.jops425.dto.DTO#fromJson()
+     */
+    @Override
+    public void fromJson() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.jops425.dto.DTO#toJson()
+     */
+    @Override
+    public void toJson() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.jops425.dto.DTO#fromXML()
+     */
+    @Override
+    public void fromXML() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.jops425.dto.DTO#toXML()
+     */
+    @Override
+    public void toXML() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
--- aula-7/dto/src/main/java/com/github/jops425/dto/package-info.java
+++ aula-7/dto/src/main/java/com/github/jops425/dto/package-info.java
@@ -1,7 +1,9 @@
 /**
- * Pacote do projeto mf-2018/aula-7/dto. Contém uma classe apenas:
+ * Pacote do projeto mf-2018/aula-7/dto. Contém duas classes:
  * ({@link com.github.jops425.dto.Main})
+ * ({@link com.github.jops425.dto.DadoDemograficoDTO}})
  *
  * @see com.github.jops425.dto.Main
+ * @see com.github.jops425.dto.DadoDemograficoDTO
  */
 package com.github.jops425.dto;
