2e9f4b3ecbeb104e11cc662dc5ee66f61fa6bae2
João Pedro Vieira <jp-a.v@hotmail.com>
2018-11-05 20:58:03 -0200

    Att package-info

--- aula-7/dto/src/main/java/com/github/jops425/dto/package-info.java
+++ aula-7/dto/src/main/java/com/github/jops425/dto/package-info.java
@@ -1,9 +1,17 @@
 /**
- * Pacote do projeto mf-2018/aula-7/dto. Contém duas classes:
+ * Pacote do projeto mf-2018/aula-7/dto. Contém seis classes:
  * ({@link com.github.jops425.dto.Main})
- * ({@link com.github.jops425.dto.DadoDemograficoDTO}})
+ * ({@link com.github.jops425.dto.DadoDemograficoDTO})
+ * ({@link com.github.jops425.dto.NomeDTO})
+ * ({@link com.github.jops425.dto.IdentificadorDTO})
+ * ({@link com.github.jops425.dto.ComunicacaoDTO})
+ * ({@link com.github.jops425.dto.VinculoDTO})
  *
  * @see com.github.jops425.dto.Main
  * @see com.github.jops425.dto.DadoDemograficoDTO
+ * @see com.github.jops425.dto.VinculoDTO
+ * @see com.github.jops425.dto.ComunicacaoDTO
+ * @see com.github.jops425.dto.IdentificadorDTO
+ * @see com.github.jops425.dto.NomeDTO
  */
 package com.github.jops425.dto;
