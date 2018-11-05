78d9c7bcecb10c614c901bcf3b4116da42915047
João Pedro Vieira <jp-a.v@hotmail.com>
2018-11-05 21:36:36 -0200

    Att IdentificadorDTO

--- aula-7/dto/src/main/java/com/github/jops425/dto/IdentificadorDTO.java
+++ aula-7/dto/src/main/java/com/github/jops425/dto/IdentificadorDTO.java
@@ -1,5 +1,35 @@
 package com.github.jops425.dto;
 
+import java.time.LocalDate;
+
 public class IdentificadorDTO {
+
+    private String designacao;
+
+    private String area;
+
+    private String emissor;
+
+    private LocalDate data;
+
+    private String tipo;
+
+    private String serie;
+
+    private String estado;
+
+    private String tipoCertidao;
+
+    private String cartorioCertidao;
+
+    private String livroCertidao;
+
+    private String folhaCertidao;
+
+    private String termoCertidao;
+
+    private String secaoTitulo;
+
+    private String zonaTitulo;
 
 }
